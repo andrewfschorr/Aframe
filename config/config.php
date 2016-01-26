225db8a3de54dd2ac08c437dc34abba834831143
Andrew Schorr <afs35mm@gmail.com>
2016-01-25 21:51:43 -0500

    password stuff

--- config/config.php
+++ config/config.php
@@ -1,7 +1,16 @@
 <?php
 date_default_timezone_set('America/New_York');
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', 'root');
-define('DB', 'aframe');
-define('CODE', 'jakesbuddy');
+
+if (ENV === 'dev') {
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', 'root');
+    define('DB', 'aframe');
+    define('CODE', 'jakesbuddy');
+} else {
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '132AllenSt!');
+    define('DB', 'aframe');
+    define('CODE', 'jakesbuddy');
+}
